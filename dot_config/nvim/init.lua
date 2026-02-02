fe0e35a43e9287dd466a7dc127a3c2a9f8e5161a
ichimei0125 <ichimei0125@gmail.com>
2026-02-02 10:21:05 +0900

    fix

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -39,16 +39,6 @@ require("lazy").setup({
         vim.g.mkdp_filetypes = { "markdown", "md" }
       end,
       ft = { "markdown", "md" },
-    },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-        ft = ['markdown'],
     }
   },
   checker = { enabled = true },
